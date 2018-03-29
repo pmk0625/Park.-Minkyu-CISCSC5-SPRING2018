6e88be32e1ae207e06ffb8a575edf357f567bb55
pmk0625 <pmk0625@hotmail.com>
2018-03-29 12:42:00 -0700

    Signed-off-by: pmk0625 <pmk0625@hotmail.com>

--- Lab/lab03292018/Gaddis_8thEd_Ch5_question7/main.cpp
+++ Lab/lab03292018/Gaddis_8thEd_Ch5_question7/main.cpp
@@ -2,11 +2,12 @@
  * File:   main.cpp
  * Author: Minkyu R. Park
  * Created on March 29, 2018, 11:20 AM
- * Purpose:  Create a CSC/CIS 5 Template
+ * Purpose:  Calculating Pennies for Pay
  */
 
 //System Libraries
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 //User Libraries
@@ -19,10 +20,23 @@ using namespace std;
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
+    unsigned int totPay, pyPrDay; //Pennies of pay
+    char numDays=31;
     
     //Initialize Variables
+    pyPrDay=1; //1 Penny on the first day
+    totPay=pyPrDay; //Total Amount
     
     //Process/Map inputs to outputs
+    cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<"Day          Pay          Total"<<endl;
+    for(int day=1;day<=numDays;day++){
+        cout<<setw(2)<<day
+        <<setw(15)<<pyPrDay/100.0f
+        <<setw(13)<<totPay/100.0f<<endl;
+        pyPrDay*=2;
+        totPay+=pyPrDay;
+    }
     
     //Output data
     
