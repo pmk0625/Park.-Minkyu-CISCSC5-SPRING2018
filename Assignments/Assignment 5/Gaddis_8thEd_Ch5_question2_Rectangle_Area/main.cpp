51a09bef9f6d9e849e7aa71ecccbb7337971ad25
pmk0625 <pmk0625@hotmail.com>
2018-05-03 12:47:12 -0700

    Signed-off-by: pmk0625 <pmk0625@hotmail.com>

--- Assignments/Assignment 5/Gaddis_8thEd_Ch5_question2_Rectangle_Area/main.cpp
+++ Assignments/Assignment 5/Gaddis_8thEd_Ch5_question2_Rectangle_Area/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Minkyu R. Park
  * Created on April 19, 2018, 11:30 AM
- * Purpose:  Create a CSC/CIS 5 Template
+ * Purpose:  Calculating Area of a Rectangle
  */
 
 //System Libraries
@@ -14,41 +14,41 @@ using namespace std;
 
 //Global Constants - Math/Physics Constants, Conversions,
 //                   2-D Array Dimensions
-const float PERCENT=100;
 
 //Function Prototypes
-int length(int); //Get Length
-int width(int); //Get Width
-float area(int, int); //Get Area
-float display(int, int, float);//Display Date
+float lg(int); //Get Length
+float wd(int); //Get Width
+float ar(int, int, float); //Get Area
 
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
+    int length, width, display;
+    float area;
     
     //Initialize Variables
+    cout<<"Input the length and width of the rectangle"<<endl;
+    cin>>length>>width;
     
     //Process/Map inputs to outputs
+    cout<<"The length = "<<lg(length)<<endl;
+    cout<<"The width = "<<wd(width)<<endl;
+    cout<<"The area = "<<ar(length, width, area)<<endl;
     
     //Output data
-    cout<<"The length = "<<length(length)<<endl;
-    cout<<"The width = "<<width(width)<<endl;
-    cout<<"The area = "<<area(length, width)<<endl;
     
     //Exit stage right!
     return 0;
 }
 
-int length (int length){
-    cin>>length;
+float lg (int length){
     return length;
 }
 
-int width (int width){
-    cin>>width;
+float wd (int width){
     return width;
 }
 
-float area (int length, int width){
-    return (length*width);
+float ar (int length, int width, float area){
+    return area=(length*width);
 }
