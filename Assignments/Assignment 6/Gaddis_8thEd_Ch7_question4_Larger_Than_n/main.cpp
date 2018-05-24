897b605498c775d1f64c60f79d4b3770436cb4be
pmk0625 <36967168+pmk0625@users.noreply.github.com>
2018-05-23 22:14:56 -0700

    Signed-off-by: pmk0625 <36967168+pmk0625@users.noreply.github.com>

--- Assignments/Assignment 6/Gaddis_8thEd_Ch7_question4_Larger_Than_n/main.cpp
+++ Assignments/Assignment 6/Gaddis_8thEd_Ch7_question4_Larger_Than_n/main.cpp
@@ -17,18 +17,19 @@ using namespace std;
 //                   2-D Array Dimensions
 
 //Function Prototypes
-void fill(int, int );
-void larger(int, int, int);
-void print(int, int);
+void fill(int[], int );
+void larger(int[], int, int);
+void print(int[], int);
 
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
-    const int size=20;
+    const int size = 20;
     int array[size];
     int n;
     
     //Initialize Variables
+    cout<<"Input a integer for n"<<endl;
     cin>>n;
     
     
@@ -39,28 +40,29 @@ int main(int argc, char** argv) {
     //Output data
     print(array, size);
     
-    
     //Exit stage right!
     return 0;
 }
 
 void fill(int array[], int size){
+    size=20;
     for(int count=0;count<size;count++){
-        array[count]=rand()%10+1;//[1,10]
+        array[count]=rand()%20+1;//[1,20]
     }
 }
 
 void larger(int array[], int size, int n){
+    size=20;
     for(int count=0;count<size;count++){
-        if(n>array[count]){
+        if(n > array[count]){
             cout<<n<<" is greater than "<<array[count]<<endl;
         }
     }
 }
 
 void print(int array[], int size){
-    cout<<"============================"<<endl;
-    cout<<"The contents of the array are : "<<endl;
+    size=20;
+    cout<<"list of random array integers used"<<endl;
     for(int count=0;count<size;count++){
         cout<<array[count]<<endl;
     }
